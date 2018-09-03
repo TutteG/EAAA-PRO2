b7d7c4bba4da65e7236fa533812a354d94c47922
Torben Grove <torben_grove@live.dk>
2018-09-03 13:21:50 +0200

    Lektion 5 - Alle opgaver næsten løst
    Opgave 4 har jeg spørgsmål til

---
+++ Lektion05-CollectionsFramework/Opgave5/spelltreeset/SpellCheck.java
@@ -0,0 +1,37 @@
+package spelltreeset;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SpellCheck {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Set<String> dictionaryWords = readWords("words");
+		// TreeSet<String> documentWords = readWords("Gjøngehøvdingen.txt");
+		TreeMap<String, Integer> words = readWords("Gjøngehøvdingen.txt");
+		Set<String> keySet = words.keySet();
+		for (String word : keySet) {
+			System.out.println(word + " " + words.get(word));
+		}
+	}
+
+	public static TreeMap<String, Integer> readWords(String filename) throws FileNotFoundException {
+		TreeMap<String, Integer> words = new TreeMap<String, Integer>();
+		Scanner in = new Scanner(new File(filename));
+		// Use any characters other than a-z or A-Z as delimiters
+		in.useDelimiter("[^a-zA-Z]+");
+		while (in.hasNext()) {
+			String niels = in.next();
+			if (words.containsKey(niels)) {
+				int something = words.get(niels);
+				words.put(niels, something + 1);
+			} else {
+				words.put(in.next(), 1);
+			}
+		}
+		in.close();
+		return words;
+	}
+}
