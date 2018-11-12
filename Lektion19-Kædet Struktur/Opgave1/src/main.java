4be8d7c963d01580b3e9714fee82b1cd7f71d284
Torben Grove <torben_grove@live.dk>
2018-11-12 12:12:45 +0100

    Lektion 19 - LinkedList
    Opgave 1

---
+++ Lektion19-Kædet Struktur/Opgave1/src/main.java
@@ -0,0 +1,19 @@
+package src;
+
+public class main {
+	public static void main(String[] args) {
+		SortedLinkedList list = new SortedLinkedList();
+		list.addElementJa("D");
+		list.addElementJa("Å");
+		list.addElementJa("Q");
+		list.addElementJa("B");
+		list.addElementJa("C");
+		list.addElementJa("A");
+		list.addElementJa("C");
+		list.addElementJa("C");
+		list.addElementJa("Q");
+		list.addElementJa("B");
+		list.addElementJa("C");
+		System.out.println(list.toString());
+	}
+}
